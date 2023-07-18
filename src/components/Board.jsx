7643af1714269386cf2fa682e69f0541f8f05874
SANDHUZZ <vashishtsandhu@gmail.com>
2023-07-18 13:19:34 +0530

    Display X when click on a square

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,21 +1,44 @@
+import { useState } from 'react';
 import Square from './square';
 const Board = () => {
+  const [square, setsquare] = useState(Array(9).fill(null));
+
+  const handleSquareClick = clickedposition => {
+    setsquare(currentsquare => {
+      return currentsquare.map((curval, indx) => {
+        if (clickedposition === indx) {
+          return 'X';
+        }
+
+        return curval;
+      });
+    });
+  };
+
+  const renderSquare = position => {
+    return (
+      <Square
+        value={square[position]}
+        onClick={() => handleSquareClick(position)}
+      />
+    );
+  };
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
